88974b2631acc1a26baca56536fcbc36e7546493
theincurablewoolgatherer <jayson_uy@ymail.com>
2014-11-14 02:57:23 +0800

    Updated UI

--- public/javascripts/skillsdb.js
+++ public/javascripts/skillsdb.js
@@ -251,6 +251,16 @@ skillsdb.service('tags', function($q, FIELD_CONSTANTS) {
   };
 
 });
-
-
+/**********************************************************************
+* Custom Filters
+**********************************************************************/
+skillsdb.filter("leftpad", function() {
+  return function(number) {
+    if (number !== null && number !== undefined) {
+      var str = "" + number;
+      while (str.length < 2) str = "0" + str;
+      return str;
+    }
+  };
+});
 
